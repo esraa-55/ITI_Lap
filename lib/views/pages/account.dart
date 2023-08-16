7d68439614dab278cdc621dcae3471a9f47ab8d8
esraa <esraarabia555@gmail.com>
2023-08-16 21:12:25 +0300

    Cloud Firestore

--- lib/views/pages/account.dart
+++ lib/views/pages/account.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class AccountScreen extends StatefulWidget {
@@ -8,11 +9,32 @@ class AccountScreen extends StatefulWidget {
 }
 
 class _AccountScreenState extends State<AccountScreen> {
+ final Stream<QuerySnapshot> productStream = FirebaseFirestore.instance.collection('Product').snapshots();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child:
-      Text("account"),),
+    return StreamBuilder<QuerySnapshot>(
+      stream: productStream ,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),);
+        }
+
+        return ListView(
+          children: snapshot.data!.docs.map((DocumentSnapshot document) {
+          Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+            return ListTile(
+              title: Text(data['name']),
+              subtitle: Text(data['price']),
+            );
+          }).toList(),
+        );
+      },
     );
   }
 }
